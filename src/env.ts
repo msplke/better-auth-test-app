14694e6878ac71e7dda91d1eab21a5db4a06126a
Mirror83 <glenochieng045@gmail.com>
2025-08-19 11:43:21 +0300

    chore(deps): add t3 env

---
+++ src/env.ts
@@ -0,0 +1,12 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string(),
+  },
+  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+});
