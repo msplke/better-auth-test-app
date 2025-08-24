107d7ef6955580f026095fc5859591065e228973
Mirror83 <glenochieng045@gmail.com>
2025-08-24 21:31:44 +0300

    chore: modify comment table and add relation to it

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { env } from "@/env";
+import { schema } from "@/db/schema";
 
-export const db = drizzle(env.DB_FILE_NAME);
+export const db = drizzle(env.DB_FILE_NAME, { schema });
--- src/db/schema/app-schema.ts
+++ src/db/schema/app-schema.ts
@@ -1,6 +1,18 @@
+import { user } from "@/db/schema/auth-schema";
+import { relations } from "drizzle-orm";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const comments = sqliteTable("comments", {
+export const comment = sqliteTable("comments", {
   id: int().primaryKey({ autoIncrement: true }),
+  authorId: text("author_id")
+    .references(() => user.id)
+    .notNull(),
   content: text().notNull(),
+  createdAt: int("created_at", { mode: "timestamp" })
+    .$defaultFn(() => /* @__PURE__ */ new Date())
+    .notNull(),
 });
+
+export const commentsRelations = relations(comment, ({ one }) => ({
+  author: one(user, { fields: [comment.authorId], references: [user.id] }),
+}));
