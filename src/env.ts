305faa3f7a4ec5ab50a8e388b4663e2320e3f50f
Mirror83 <glenochieng045@gmail.com>
2025-08-28 09:49:36 +0300

    chore: manually specify better-auth base url for deployment to vercel

--- src/env.ts
+++ src/env.ts
@@ -1,16 +1,15 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { vercel } from "@t3-oss/env-nextjs/presets-zod";
 import { z } from "zod";
 
 export const env = createEnv({
+  extends: [vercel()],
   server: {
     DATABASE_URL: z.string(),
     BETTER_AUTH_SECRET: z.string().length(32),
-    BETTER_AUTH_URL: z.url(),
   },
-  // If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
   },
 });
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,12 +2,20 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db";
 import { schema } from "@/db/schema";
+import { env } from "@/env";
+
+const baseUrl =
+  env.VERCEL_ENV === "production"
+    ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : env.VERCEL_ENV === "preview"
+    ? `https://${env.VERCEL_URL}`
+    : "http://localhost:3000";
 
 export const auth = betterAuth({
+  baseURL: baseUrl,
   emailAndPassword: {
     enabled: true,
   },
-
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite
     schema,
